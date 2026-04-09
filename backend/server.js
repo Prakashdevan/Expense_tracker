38db611c413167d258246c8aaee91d2fb78b52e9
Prakashdevan <prakash337424@gmail.com>
2026-04-09 12:39:55 +0530

    new version

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ mongoose
   .then(() => {
     console.log('Connected to MongoDB');
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
